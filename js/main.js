b5e1591fde85f43e9905c0b61cf096b4fe5a9544
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-04 11:38:20 -0300

    Configurei o nome da cidade e o ceu(3).

--- js/main.js
+++ js/main.js
@@ -1,36 +1,65 @@
 const key = '40a456493bf084075887ae0045fb36d9'
-const p_data = document.querySelector('#p_data')
 const input = document.getElementsByTagName('input')[0]
+const btn = document.getElementsByTagName('button')[0]
+
+function fahrenheitParaCelsius(fahrenheit) {
+    // Aplicando a fórmula de conversão
+    var celsius = (5 / 9) * (fahrenheit - 32);
+    return celsius;
+}
 
 
-const f = async() =>{
+const get_weather = async(cidade) =>{
     try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=SERGIPE,SE,BR&lang=pt_br&appid=${key}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade},SE,BR&lang=pt_br&appid=${key}`)
         const data = await response.json()
 
-
-        // Configurando a data
+        // Congigurando a data
         const tempo = new Date()
+         
         let dia = tempo.getDate()
-        dia = dia<10?'0'+dia:dia
-       
+        dia = dia < 10? '0'+ dia : dia
+
         let mes = tempo.getMonth()
-        mes = mes<10?'0'+mes:mes
+        mes = mes < 10? '0'+ mes : mes
 
         let ano = tempo.getFullYear()
 
-        
+        const container = document.querySelector('.container')
+        container.style.display = 'flex'
+        container.classList.add('border')
+        container.innerHTML = ''
+
+        const p_data = document.createElement('p')
         p_data.innerHTML = `${dia}/${mes}/${ano}`
 
+        const p_cidade = document.createElement('p')
+        p_cidade.innerHTML = data.name
 
+        const grau = document.createElement('h1')
+        grau.innerHTML = '25'+'º'
 
-        // console.log(data)
-        // console.log(data.name)
+        const p_ceu = document.createElement('p')
+        p_ceu.innerHTML = data.weather[0].description
+
+        container.appendChild(p_data)
+        container.appendChild(p_cidade)
+        container.appendChild(grau)
+        container.appendChild(p_ceu)
+
+       
         // console.log(data.main.temp)
-        // console.log(data.weather[0].description)
+        
     }catch(err){
         console.log(err)
     }
 } 
 
-f()
+btn.addEventListener('click',()=>{
+    const nome = input.value
+    if(nome===''){
+        alert('Por favor dogite um nome de uma cidade')
+    } else {
+        get_weather(nome)
+    }
+})
