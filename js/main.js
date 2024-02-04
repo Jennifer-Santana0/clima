06bc0f473a7592960c1c814e4c87a131a1b2c46c
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-04 10:58:14 -0300

    Configurei a data(2).

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 const key = '40a456493bf084075887ae0045fb36d9'
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=
+const p_data = document.querySelector('#p_data')
+const input = document.getElementsByTagName('input')[0]
 
-const a_1 = `https://api.openweathermap.org/data/2.5/weather?q=SERGIPE,SE,BR&lang=pt_br&appid=${key}`
 
 const f = async() =>{
     try{
-        const response = await fetch(a_1)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=SERGIPE,SE,BR&lang=pt_br&appid=${key}`)
         const data = await response.json()
-        console.log(data)
-        console.log(data.name)
-        console.log(data.main.temp)
-        console.log(data.weather[0].description)
+
+
+        // Configurando a data
+        const tempo = new Date()
+        let dia = tempo.getDate()
+        dia = dia<10?'0'+dia:dia
+       
+        let mes = tempo.getMonth()
+        mes = mes<10?'0'+mes:mes
+
+        let ano = tempo.getFullYear()
+
+        
+        p_data.innerHTML = `${dia}/${mes}/${ano}`
+
+
+
+        // console.log(data)
+        // console.log(data.name)
+        // console.log(data.main.temp)
+        // console.log(data.weather[0].description)
     }catch(err){
         console.log(err)
     }
